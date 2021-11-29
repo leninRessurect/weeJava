1de2302f612f1167f02e483dea6667b2d5b5b6b2
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2021-11-29 09:09:21 +0000

    dev

--- weeJava/src/Q1Tester.java
+++ weeJava/src/Q1Tester.java
@@ -1,5 +1,11 @@
 public class Q1Tester {
-    public static void test_getOp_null() {
+    public static void main(String[] args) {
+        
+    }
 
+    public static void getSymbol_returnNull() {
+        TokenType t = Q1.getSymbol('m');
+        System.out.println(t);
+        
     }
 }
